94d9d78cf2c643eac6cf119f612d01ca0f82c089
Jan Schenk <Jan.Schenk@swisscom.com>
2021-02-25 16:04:10 +0100

    First implementation of NavigationBar.

--- client/src/shell/NavigationBar.tsx
+++ client/src/shell/NavigationBar.tsx
@@ -1,3 +1,30 @@
+import { BottomNavigation, BottomNavigationAction, makeStyles } from "@material-ui/core";
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+//import MonetizationOnIcon from '@material-ui/icons/MonetizationOn'; // TODO js (25.02.2021): Alternative icon for rewards.
+import RedeemIcon from '@material-ui/icons/Redeem';
+
+const useStyles = makeStyles({
+    navigation: {
+        backgroundColor: "#01579b", // TODO js (25.02.2021): Move color selection to theme?
+        color: "#fff", // TODO js (25.02.2021): Move color selection to theme?
+    },
+    action: {
+        color: "#fff", // TODO js (25.02.2021): Move color selection to theme?
+    },
+    icon: {
+        color: "#fff", // TODO js (25.02.2021): Move color selection to theme?
+    }
+});
+
 export function NavigationBar() {
-    return <p>NavigationBar</p>
+    const classes = useStyles();
+
+    return (
+        <BottomNavigation className={classes.navigation} showLabels>
+            <BottomNavigationAction className={classes.action} label="Ämtli" icon={<AssignmentIcon className={classes.icon} />} />
+            <BottomNavigationAction className={classes.action} label="Belohnungen" icon={<RedeemIcon className={classes.icon} />} />
+            <BottomNavigationAction className={classes.action} label="Profil" icon={<AssignmentIndIcon className={classes.icon} />} />
+        </BottomNavigation>
+    );
 }
