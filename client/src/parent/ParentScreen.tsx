167888617f8a26f67c12d37c5680aa6548332b6b
Jan Schenk <Jan.Schenk@swisscom.com>
2021-02-27 09:29:50 +0100

    Added routes for the different screens on the parent dashboard.

--- client/src/parent/ParentScreen.tsx
+++ client/src/parent/ParentScreen.tsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import { DashboardLayout } from "../shell/DashboardLayout";
 import { ChoreTable } from "./ChoreTable";
+import { BrowserRouter as  Switch, Route, useRouteMatch } from "react-router-dom";
 
 export function ParentScreen() {
-  const [title, ] = useState("Ämtli Pinwand"); // TODO js (25.02.2021): Set title depending on state of the app.
+  const {path, } = useRouteMatch();
   const [avatarAlt, ] = useState("Mami"); // TODO js (25.02.2021): Set the avatar alt value depending on logged in user.
   const [avatarSrc, ] = useState(""); // TODO js (25.02.2021): Set the avatar src depending on logged in user.
 
   return (
-    <DashboardLayout avatarAlt={avatarAlt} avatarSrc={avatarSrc} title={title}>
-      <ChoreTable/>
-    </DashboardLayout>
+    <Switch>
+      <Route path={`${path}/rewards`}>
+        <DashboardLayout avatarAlt={avatarAlt} avatarSrc={avatarSrc} title="Belohungs-Pinwand">
+          <p>Belohnungen...</p>
+        </DashboardLayout>
+      </Route>
+      <Route path={`${path}/profile`}>
+        <DashboardLayout avatarAlt={avatarAlt} avatarSrc={avatarSrc} title="Profil">
+          <p>Profil...</p>
+        </DashboardLayout>
+      </Route>
+      <Route path={path}>
+        <DashboardLayout avatarAlt={avatarAlt} avatarSrc={avatarSrc} title="Ämtli Pinwand">
+          <ChoreTable/>
+        </DashboardLayout>
+      </Route>
+    </Switch>
   );
 }
