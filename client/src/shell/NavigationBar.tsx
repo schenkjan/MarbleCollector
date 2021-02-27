6eb6993d59fd98cb0720d6922215462b9bd3dab2
Jan Schenk <Jan.Schenk@swisscom.com>
2021-02-27 17:04:57 +0100

    Added badges to navigation icons. Handling navigation icon badge state using Recoil.

--- client/src/shell/NavigationBar.tsx
+++ client/src/shell/NavigationBar.tsx
@@ -1,10 +1,11 @@
-import { BottomNavigation, BottomNavigationAction, makeStyles } from "@material-ui/core";
+import { Badge, BottomNavigation, BottomNavigationAction, makeStyles } from "@material-ui/core";
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 //import MonetizationOnIcon from '@material-ui/icons/MonetizationOn'; // TODO js (25.02.2021): Alternative icon for rewards.
 import RedeemIcon from '@material-ui/icons/Redeem';
 import { useEffect, useState } from "react";
 import { Link, useRouteMatch } from "react-router-dom";
+import { atom, useRecoilState } from "recoil";
 import { useDashboardBasePath } from "./DashboardBasePathHook";
 
 const useStyles = makeStyles({
@@ -20,11 +21,32 @@ const useStyles = makeStyles({
     }
 });
 
+// TODO js (27.02.2021): Should we move the state handling code to a store file?
+export const choreNewsCount = atom<number>({
+    key: "choreNewsCount",
+    default: 10, // TODO js 27.02.2021: Set correct value.
+});
+
+// TODO js (27.02.2021): Should we move the state handling code to a store file?
+export const rewardNewsCount = atom<number>({
+    key: "rewardNewsCount",
+    default: 100, // TODO js 27.02.2021: Set correct value.
+});
+
+// TODO js (27.02.2021): Should we move the state handling code to a store file?
+export const profileNewsCount = atom<number>({
+    key: "profileNewsCount",
+    default: 1, // TODO js 27.02.2021: Set correct value.
+});
+
 export function NavigationBar() {
     const classes = useStyles();
     const [value, setValue] = useState<number>();
     const {path, } = useRouteMatch();
     const dashboardBasePath = useDashboardBasePath();
+    const [choreNews, setChoreNews] = useRecoilState(choreNewsCount);
+    const [rewardNews, setRewardNews] = useRecoilState(rewardNewsCount);
+    const [profileNews, setProfileNews] = useRecoilState(profileNewsCount);
 
     useEffect(() => {
         if (path.match(/\/rewards/)) {
@@ -42,6 +64,7 @@ export function NavigationBar() {
         setValue(newValue);
     }
 
+    // TODO js (27.02.2021): Consider to improve onClick handlers!
     return (
         <BottomNavigation 
             className={classes.navigation}
@@ -51,23 +74,32 @@ export function NavigationBar() {
             <BottomNavigationAction 
                 className={classes.action} 
                 label="Ämtli" 
-                icon={<AssignmentIcon className={classes.icon}/>}
+                icon={
+                    <Badge badgeContent={choreNews} color="secondary"><AssignmentIcon className={classes.icon} /></Badge>
+                }
                 component={Link}
                 to={`${dashboardBasePath}/chores`}
+                onClick={() => setChoreNews(0)}
             />
             <BottomNavigationAction 
                 className={classes.action} 
                 label="Belohnungen" 
-                icon={<RedeemIcon className={classes.icon} />}
+                icon={
+                    <Badge badgeContent={rewardNews} color="secondary"><RedeemIcon className={classes.icon} /></Badge>
+                }
                 component={Link}
                 to={`${dashboardBasePath}/rewards`}
+                onClick={() => setRewardNews(0)}
             />
             <BottomNavigationAction 
                 className={classes.action} 
                 label="Profil" 
-                icon={<AssignmentIndIcon className={classes.icon} />}
+                icon={
+                    <Badge badgeContent={profileNews} color="secondary"><AssignmentIndIcon className={classes.icon} /></Badge>
+                }
                 component={Link}
                 to={`${dashboardBasePath}/profile`}
+                onClick={() => setProfileNews(0)}
             />
         </BottomNavigation>
     );
