0727ee73ba1b9ff81a217eae37825b7ee7734244
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-04 13:49:18 +0100

    Added repository skeleton for assignment and grant.

---
+++ api/MarbleCollectorApi/Data/Repository/IGrantRepository.cs
@@ -0,0 +1,10 @@
+﻿using MarbleCollectorApi.Data.Models;
+using MarbleCollectorApi.Data.Repository.Core;
+
+namespace MarbleCollectorApi.Data.Repository
+{
+    public interface IGrantRepository : IBaseEntityRepository<Grant>
+    {
+        
+    }
+}
