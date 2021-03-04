936a1b7e54fda6628764964398fd7e7193ed787c
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-04 17:39:21 +0100

    Updated chore table to update data model.

--- client/src/parent/ChoreTable.tsx
+++ client/src/parent/ChoreTable.tsx
@@ -1,169 +1,209 @@
-import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, makeStyles, createStyles, Theme } from "@material-ui/core";
+import {
+  Paper,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  makeStyles,
+  createStyles,
+  Theme,
+} from "@material-ui/core";
 import { useEffect, useState } from "react";
-import { Chore } from "../model/Chore";
 import { ChoreTableRow } from "./ChoreTableRow";
 import { Fab } from "@material-ui/core";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
+import { ChoreWithAssignments } from "./models/ChoreWithAssignments";
+import { AssignmentState } from "./models/AssignmentState";
 
 // TODO js (25.02.2021): Remove dummy data as soon as data is consumed from backend.
 const lars = {
-    id: 1,
-    name: "Lars",
-    avatarSrc: ""
- };
- const lara = {
-    id: 2,
-    name: "Lara",
-    avatarSrc: ""
- };
- const lena = {
-    id: 3,
-    name: "Lena",
-    avatarSrc: ""
- };
+  id: 1,
+  name: "Lars",
+  avatarSrc: "",
+};
+const lara = {
+  id: 2,
+  name: "Lara",
+  avatarSrc: "",
+};
+const lena = {
+  id: 3,
+  name: "Lena",
+  avatarSrc: "",
+};
 
- const useStyles = makeStyles((theme: Theme) => 
-    createStyles({
-     container: {
-         flex: "1 1 auto",
-     },
-     fab: {
-        position: 'absolute',
-        bottom: theme.spacing(6),
-        right: theme.spacing(2),
-      },
-    }),
- );
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      flex: "1 1 auto",
+    },
+    fab: {
+      position: "absolute",
+      bottom: theme.spacing(6),
+      right: theme.spacing(2),
+    },
+  })
+);
 
 export function ChoreTable() {
-    const [chores, setChores] = useState<Chore[]>([]);
-    const classes = useStyles();
+  const [chores, setChores] = useState<ChoreWithAssignments[]>([]);
+  const classes = useStyles();
 
-    useEffect(() => {
-        setChores([ // TODO js (25.02.2021): Remove dummy data as soon as data is consumed from backend.
-            {
-                id: 42, 
-                name: "Rasen mähen",
-                description: "", 
-                dueDate: Date.now(),
-                value: 20,
-                assignments: [
-                    {
-                        isDone: true,
-                        isConfirmed: false,
-                        assignee: lara
-                    }
-                ],
-            },
-            {
-                id: 1, 
-                name: "Zimmer staubsaugen",
-                description: "", 
-                dueDate: Date.now(),
-                value: 10, 
-                assignments: [
-                    {
-                        isDone: false,
-                        isConfirmed: false,
-                        assignee: lars
-                    },
-                    {
-                        isDone: true,
-                        isConfirmed: false,
-                        assignee: lara
-                    },
-                    {
-                        isDone: true,
-                        isConfirmed: true,
-                        assignee: lena
-                    }
-                ]
-            },
-            {
-                id: 2, 
-                name: "Abfall rausbringen",
-                description: "", 
-                dueDate: Date.now(),
-                value: 5,
-                assignments: [
-                    {
-                        isDone: true,
-                        isConfirmed: false,
-                        assignee: lara
-                    },
-                    {
-                        isDone: false,
-                        isConfirmed: false,
-                        assignee: lena
-                    }
-                ],
-            },
-            {
-                id: 3, 
-                name: "Abwaschen",
-                description: "", 
-                dueDate: Date.now(),
-                value: 5,
-                assignments: [
-                    {
-                        isDone: true,
-                        isConfirmed: false,
-                        assignee: lara
-                    },
-                    {
-                        isDone: false,
-                        isConfirmed: false,
-                        assignee: lena
-                    }
-                ],
-            },
-            {
-                id: 4, 
-                name: "Zimmer aufräumen",
-                description: "", 
-                dueDate: Date.now(),
-                value: 10, 
-                assignments: [
-                    {
-                        isDone: false,
-                        isConfirmed: false,
-                        assignee: lars
-                    },
-                    {
-                        isDone: true,
-                        isConfirmed: false,
-                        assignee: lara
-                    },
-                    {
-                        isDone: true,
-                        isConfirmed: true,
-                        assignee: lena
-                    }
-                ]
-            },
-        ]);   
-    }, []);
+  useEffect(() => {
+    setChores([
+      // TODO js (25.02.2021): Remove dummy data as soon as data is consumed from backend.
+      {
+        id: 42,
+        name: "Rasen mähen",
+        description: "",
+        dueDate: new Date(Date.now()),
+        value: 20,
+        assignments: [
+          {
+            id: 1,
+            state: AssignmentState.RequestedToCheck,
+            userId: lara.id,
+            userName: lara.name,
+            choreId: 42,
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Zimmer staubsaugen",
+        description: "",
+        dueDate: new Date(Date.now()),
+        value: 10,
+        assignments: [
+          {
+            id: 2,
+            state: AssignmentState.Assigned,
+            userId: lars.id,
+            userName: lars.name,
+            choreId: 1,
+          },
+          {
+            id: 3,
+            state: AssignmentState.RequestedToCheck,
+            userId: lara.id,
+            userName: lara.name,
+            choreId: 1,
+          },
+          {
+            id: 4,
+            state: AssignmentState.CheckConfirmed,
+            userId: lena.id,
+            userName: lena.name,
+            choreId: 1,
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Abfall rausbringen",
+        description: "",
+        dueDate: new Date(Date.now()),
+        value: 5,
+        assignments: [
+          {
+            id: 5,
+            state: AssignmentState.RequestedToCheck,
+            userId: lena.id,
+            userName: lena.name,
+            choreId: 2,
+          },
+          {
+            id: 6,
+            state: AssignmentState.Assigned,
+            userId: lara.id,
+            userName: lara.name,
+            choreId: 2,
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Abwaschen",
+        description: "",
+        dueDate: new Date(Date.now()),
+        value: 5,
+        assignments: [
+          {
+            id: 7,
+            state: AssignmentState.RequestedToCheck,
+            userId: lars.id,
+            userName: lars.name,
+            choreId: 3,
+          },
+          {
+            id: 8,
+            state: AssignmentState.Assigned,
+            userId: lara.id,
+            userName: lara.name,
+            choreId: 3,
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Zimmer aufräumen",
+        description: "",
+        dueDate: new Date(Date.now()),
+        value: 10,
+        assignments: [
+          {
+            id: 9,
+            state: AssignmentState.Active,
+            userId: lars.id,
+            userName: lars.name,
+            choreId: 4,
+          },
+          {
+            id: 10,
+            state: AssignmentState.RequestedToCheck,
+            userId: lara.id,
+            userName: lara.name,
+            choreId: 4,
+          },
+          {
+            id: 11,
+            state: AssignmentState.Archived,
+            userId: lena.id,
+            userName: lena.name,
+            choreId: 4,
+          },
+        ],
+      },
+    ]);
+  }, []);
 
-    return (
-        <TableContainer className={classes.container} component={Paper}>
-            <Table stickyHeader aria-label="sticky table" size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Ämtli</TableCell>
-                        <TableCell></TableCell>
-                        <TableCell align="center">Done</TableCell>
-                        <TableCell align="center">Bestätigen</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {chores.map((chore) => (
-                        <ChoreTableRow key={chore.id} chore={chore} />
-                    ))}
-                </TableBody>
-            </Table>
-            <Fab className={classes.fab} size="small" color="primary" aria-label="add">
-                <AddIcon />
-            </Fab>
-        </TableContainer>
-    );
+  return (
+    <TableContainer className={classes.container} component={Paper}>
+      <Table stickyHeader aria-label="sticky table" size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Ämtli</TableCell>
+            <TableCell></TableCell>
+            <TableCell align="center">Done</TableCell>
+            <TableCell align="center">Bestätigen</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {chores.map((chore) => (
+            <ChoreTableRow key={chore.id} chore={chore} />
+          ))}
+        </TableBody>
+      </Table>
+      <Fab
+        className={classes.fab}
+        size="small"
+        color="primary"
+        aria-label="add"
+      >
+        <AddIcon />
+      </Fab>
+    </TableContainer>
+  );
 }
--- client/src/parent/ChoreTableRow.tsx
+++ client/src/parent/ChoreTableRow.tsx
@@ -1,43 +1,88 @@
-import { TableRow, TableCell, Checkbox, Button, makeStyles } from "@material-ui/core";
-import { Chore } from "../model/Chore";
+import {
+  TableRow,
+  TableCell,
+  Checkbox,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import { AssignmentState } from "./models/AssignmentState";
+import { ChoreWithAssignments } from "./models/ChoreWithAssignments";
 
 const useStyles = makeStyles({
-    row: {
-        "& > *": {
-            borderBottom: "unset",
-        },
+  row: {
+    "& > *": {
+      borderBottom: "unset",
     },
-    assignmentRow: {
-        "& > *": {
-            borderBottom: "unset",
-        },
+  },
+  assignmentRow: {
+    "& > *": {
+      borderBottom: "unset",
     },
+  },
 });
 
 type Prop = {
-    chore: Chore
-}
+  chore: ChoreWithAssignments;
+};
 
 export function ChoreTableRow(props: Prop) {
-    const classes = useStyles();
+  const classes = useStyles();
 
+  function isDone(state: AssignmentState): boolean {
     return (
-        <>
-            <TableRow className={classes.row} key={props.chore.id}>
-                <TableCell component="th" scope="row" colSpan={4}>
-                    {props.chore.name}
-                </TableCell>
-            </TableRow>
-            {props.chore.assignments.map((assignment, index) => {
-                return (
-                    <TableRow className={index !== props.chore.assignments.length - 1 ? classes.assignmentRow : ""} key={`${props.chore.id}${assignment.assignee.id}`} >
-                        <TableCell component="th" scope="row"></TableCell>
-                        <TableCell align="left">{assignment.assignee.name}</TableCell>
-                        <TableCell align="center"><Checkbox checked={assignment.isDone} disabled size="small"/></TableCell>
-                        <TableCell align="center">{assignment.isDone && !assignment.isConfirmed ? <Button variant="contained" color="primary" size="small">Ok</Button> : ""}</TableCell>
-                    </TableRow>
-                );
-            })}
-        </>
+      state === AssignmentState.RequestedToCheck ||
+      state === AssignmentState.CheckConfirmed ||
+      state === AssignmentState.CheckRefused ||
+      state === AssignmentState.Archived
     );
+  }
+
+  function isConfirmed(state: AssignmentState): boolean {
+    return (
+      state === AssignmentState.CheckConfirmed ||
+      state === AssignmentState.CheckRefused ||
+      state === AssignmentState.Archived
+    );
+  }
+
+  return (
+    <>
+      <TableRow className={classes.row} key={props.chore.id}>
+        <TableCell component="th" scope="row" colSpan={4}>
+          {props.chore.name}
+        </TableCell>
+      </TableRow>
+      {props.chore.assignments.map((assignment, index) => {
+        return (
+          <TableRow
+            className={
+              index !== props.chore.assignments.length - 1
+                ? classes.assignmentRow
+                : ""
+            }
+            key={`${props.chore.id}${assignment.userId}`}
+          >
+            <TableCell component="th" scope="row"></TableCell>
+            <TableCell align="left">{assignment.userName}</TableCell>
+            <TableCell align="center">
+              <Checkbox
+                checked={isDone(assignment.state)}
+                disabled
+                size="small"
+              />
+            </TableCell>
+            <TableCell align="center">
+              {isDone(assignment.state) && !isConfirmed(assignment.state) ? (
+                <Button variant="contained" color="primary" size="small">
+                  Ok
+                </Button>
+              ) : (
+                ""
+              )}
+            </TableCell>
+          </TableRow>
+        );
+      })}
+    </>
+  );
 }
