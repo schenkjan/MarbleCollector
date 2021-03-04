01fbf4698787edb464c980890ad8668f2db3ee0a
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-04 22:27:24 +0100

    Introduced QueryClientProvider for React Query. Reading chore data from backend using React Query and axios.

--- client/src/parent/ChoreTable.tsx
+++ client/src/parent/ChoreTable.tsx
@@ -10,29 +10,14 @@ import {
   createStyles,
   Theme,
 } from "@material-ui/core";
-import { useEffect, useState } from "react";
 import { ChoreTableRow } from "./ChoreTableRow";
 import { Fab } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import { ChoreWithAssignments } from "./models/ChoreWithAssignments";
-import { AssignmentState } from "./models/AssignmentState";
-
-// TODO js (25.02.2021): Remove dummy data as soon as data is consumed from backend.
-const lars = {
-  id: 1,
-  name: "Lars",
-  avatarSrc: "",
-};
-const lara = {
-  id: 2,
-  name: "Lara",
-  avatarSrc: "",
-};
-const lena = {
-  id: 3,
-  name: "Lena",
-  avatarSrc: "",
-};
+import { useQuery } from "react-query";
+import axios from "axios";
+import { useRecoilValue } from "recoil";
+import { AppState } from "../AppState";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -47,137 +32,23 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export function ChoreTable() {
-  const [chores, setChores] = useState<ChoreWithAssignments[]>([]);
+const apiBaseUrl = process.env.REACT_APP_APIBASEURL as string;
+
+export function ChoreTable(): JSX.Element {
   const classes = useStyles();
 
-  useEffect(() => {
-    setChores([
-      // TODO js (25.02.2021): Remove dummy data as soon as data is consumed from backend.
-      {
-        id: 42,
-        name: "Rasen mähen",
-        description: "",
-        dueDate: new Date(Date.now()),
-        value: 20,
-        assignments: [
-          {
-            id: 1,
-            state: AssignmentState.RequestedToCheck,
-            userId: lara.id,
-            userName: lara.name,
-            choreId: 42,
-          },
-        ],
+  const bearerToken = useRecoilValue(AppState.userBearerToken);
+  const { isLoading, error, data } = useQuery("choreData", () =>
+    axios.get<ChoreWithAssignments[]>(`${apiBaseUrl}/api/Chores/Assignments`, {
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
       },
-      {
-        id: 1,
-        name: "Zimmer staubsaugen",
-        description: "",
-        dueDate: new Date(Date.now()),
-        value: 10,
-        assignments: [
-          {
-            id: 2,
-            state: AssignmentState.Assigned,
-            userId: lars.id,
-            userName: lars.name,
-            choreId: 1,
-          },
-          {
-            id: 3,
-            state: AssignmentState.RequestedToCheck,
-            userId: lara.id,
-            userName: lara.name,
-            choreId: 1,
-          },
-          {
-            id: 4,
-            state: AssignmentState.CheckConfirmed,
-            userId: lena.id,
-            userName: lena.name,
-            choreId: 1,
-          },
-        ],
-      },
-      {
-        id: 2,
-        name: "Abfall rausbringen",
-        description: "",
-        dueDate: new Date(Date.now()),
-        value: 5,
-        assignments: [
-          {
-            id: 5,
-            state: AssignmentState.RequestedToCheck,
-            userId: lena.id,
-            userName: lena.name,
-            choreId: 2,
-          },
-          {
-            id: 6,
-            state: AssignmentState.Assigned,
-            userId: lara.id,
-            userName: lara.name,
-            choreId: 2,
-          },
-        ],
-      },
-      {
-        id: 3,
-        name: "Abwaschen",
-        description: "",
-        dueDate: new Date(Date.now()),
-        value: 5,
-        assignments: [
-          {
-            id: 7,
-            state: AssignmentState.RequestedToCheck,
-            userId: lars.id,
-            userName: lars.name,
-            choreId: 3,
-          },
-          {
-            id: 8,
-            state: AssignmentState.Assigned,
-            userId: lara.id,
-            userName: lara.name,
-            choreId: 3,
-          },
-        ],
-      },
-      {
-        id: 4,
-        name: "Zimmer aufräumen",
-        description: "",
-        dueDate: new Date(Date.now()),
-        value: 10,
-        assignments: [
-          {
-            id: 9,
-            state: AssignmentState.Active,
-            userId: lars.id,
-            userName: lars.name,
-            choreId: 4,
-          },
-          {
-            id: 10,
-            state: AssignmentState.RequestedToCheck,
-            userId: lara.id,
-            userName: lara.name,
-            choreId: 4,
-          },
-          {
-            id: 11,
-            state: AssignmentState.Archived,
-            userId: lena.id,
-            userName: lena.name,
-            choreId: 4,
-          },
-        ],
-      },
-    ]);
-  }, []);
+    })
+  );
+
+  if (isLoading) return <p>Loading...</p>; // TODO js (04.03.2021): Implement more sophisticated loading screen.
+
+  if (error) return <p>{`An error has occurred: ${error}`}</p>; // TODO js (04.03.2021): Implement more sophisticated error screen.
 
   return (
     <TableContainer className={classes.container} component={Paper}>
@@ -191,7 +62,7 @@ export function ChoreTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {chores.map((chore) => (
+          {data?.data.map((chore) => (
             <ChoreTableRow key={chore.id} chore={chore} />
           ))}
         </TableBody>
