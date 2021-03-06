1ca7c8db062ca92fabd661b72f885831e853258a
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-06 09:43:35 +0100

    Implemented grant repository.

--- api/MarbleCollectorApi/Data/Repository/GrantRepository.cs
+++ api/MarbleCollectorApi/Data/Repository/GrantRepository.cs
@@ -1,10 +1,18 @@
-﻿using MarbleCollectorApi.Data.Models;
+﻿using System.Collections.Generic;
+using MarbleCollectorApi.Data.Models;
 using MarbleCollectorApi.Data.Repository.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace MarbleCollectorApi.Data.Repository
 {
     public class GrantRepository : BaseEntityRepository<Grant>, IGrantRepository
     {
         public GrantRepository(MarbleCollectorDbContext context) : base(context) { }
+
+        public override IEnumerable<Grant> GetAll()
+        {
+            return Context.Grants
+                .Include(grant => grant.User);
+        }
     }
 }
