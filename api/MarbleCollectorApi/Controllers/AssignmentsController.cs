86f415edd8be3f3d042cc0d373b2902e3dcc59d6
severin.hofer <00severin@gmx.net>
2021-03-07 20:27:21 +0100

    first test implementation for assignment changed notification via signalR hub

--- api/MarbleCollectorApi/Controllers/AssignmentsController.cs
+++ api/MarbleCollectorApi/Controllers/AssignmentsController.cs
@@ -8,6 +8,10 @@ using MarbleCollectorApi.Data.Repository;
 using MarbleCollectorApi.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.AspNetCore.SignalR;
+using MarbleCollectorApi.Hubs;
+using MarbleCollectorApi.Services.Background;
+using System.Threading.Tasks;
 
 namespace MarbleCollectorApi.Controllers
 {
@@ -16,11 +20,15 @@ namespace MarbleCollectorApi.Controllers
     public class AssignmentsController : Controller
     {
         private readonly IAssignmentRepository _assignmentRepository;
+        private readonly IHubContext<ParentNotificationHub> _parentNotificationHubContext;
+        private readonly IHubContext<ChildrenNotificationHub> _childrenNotificationHubContext;
 
-        public AssignmentsController(IAssignmentRepository assignmentRepository)
+        public AssignmentsController(IAssignmentRepository assignmentRepository, IHubContext<ParentNotificationHub> parentNotificationHubContext, IHubContext<ChildrenNotificationHub> childrenNotificationHubContext)
         {
             _assignmentRepository =
                 assignmentRepository ?? throw new ArgumentNullException(nameof(assignmentRepository));
+            _parentNotificationHubContext = parentNotificationHubContext;
+            _childrenNotificationHubContext = childrenNotificationHubContext;
         }
 
         // TODO js (04.03.2021): Can all users get all assignments?
@@ -72,11 +80,23 @@ namespace MarbleCollectorApi.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<Assignment> UpdateAssignment(int id, Assignment assignment)
+        public async Task<ActionResult<Assignment>> UpdateAssignment(int id, Assignment assignment)
         {
             if (id != assignment.Id)
             {
                 return BadRequest();
+            }
+
+            if ((int)assignment.State != (int)_assignmentRepository.GetSingle(id).State)
+            {
+                if (assignment.State == AssignmentState.Assigned || assignment.State == AssignmentState.CheckConfirmed || assignment.State == AssignmentState.CheckRefused)
+                {
+                    await _childrenNotificationHubContext.Clients.All.SendAsync("UpdateAssignments", assignment.UserId, assignment.ChoreId);
+                }
+                else
+                {
+                    await _parentNotificationHubContext.Clients.All.SendAsync("UpdateAssignments", assignment.UserId, assignment.ChoreId);
+                }
             }
 
             // TODO hs 210307, can a assignment be modified if the state is Archived, which is by defintion the final state
