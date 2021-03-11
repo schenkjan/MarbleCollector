cb473f41e7a00ec94bf08c14f108619d90edede8
David <vid_aesch@hotmail.com>
2021-03-11 17:46:32 +0100

    - Simplified the role based routing - Ensured that parents can only access parent dashboard and vis versa

--- client/src/parent/ParentScreen.tsx
+++ client/src/parent/ParentScreen.tsx
@@ -1,22 +1,27 @@
+import React from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import { ProfileDetails } from "./profile/ProfileDetails";
 import { RewardsTable } from "./rewards/RewardsTable";
 import { ChoreList } from "./ChoreList";
+import { ProtectedRouteForRole } from "../auth/ProtectedRouteForRole";
 
 export function ParentScreen() {
   const { path } = useRouteMatch();
 
   return (
-    <Switch>
-      <Route path={`${path}/rewards`}>
-        <RewardsTable />
-      </Route>
-      <Route path={`${path}/profile`}>
-        <ProfileDetails />
-      </Route>
-      <Route path={[`${path}/chores`, path]} exact>
-        <ChoreList />
-      </Route>
-    </Switch>
+    <>
+      <ProtectedRouteForRole />
+      <Switch>
+        <Route path={`${path}/rewards`}>
+          <RewardsTable />
+        </Route>
+        <Route path={`${path}/profile`}>
+          <ProfileDetails />
+        </Route>
+        <Route path={[`${path}/chores`, path]} exact>
+          <ChoreList />
+        </Route>
+      </Switch>
+    </>
   );
 }
