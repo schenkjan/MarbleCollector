b3e6f64e29b46c3ef56ccd85cf31a28af324194e
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-11 17:28:12 +0100

    Updated missing property in Grant view model. Added RewardWithGrants view model. Added typescript equivalents for the view models.

---
+++ api/MarbleCollectorApi/ViewModels/RewardWithGrants.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MarbleCollectorApi.ViewModels
+{
+    public class RewardWithGrants
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Value { get; set; }
+
+        public IEnumerable<Grant> Grants { get; set; }
+    }
+}
