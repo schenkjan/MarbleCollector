da56c05d27095182a8d0a6e385977eeb75987ac9
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-18 07:44:24 +0100

    Updated create grant method to correctly load user and reward information. Added notifications for children and parent hubs.

---
+++ api/MarbleCollectorApi/Data/Migrations/20210318060640_UniqueIndexForGrants.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MarbleCollectorApi.Data.Migrations
+{
+    public partial class UniqueIndexForGrants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Grants_UserId",
+                table: "Grants");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grants_UserId_RewardId",
+                table: "Grants",
+                columns: new[] { "UserId", "RewardId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Grants_UserId_RewardId",
+                table: "Grants");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grants_UserId",
+                table: "Grants",
+                column: "UserId");
+        }
+    }
+}
