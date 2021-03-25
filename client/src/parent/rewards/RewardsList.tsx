d39efa377ea845f5be70f8f0a23e05676fb6893b
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-25 09:23:22 +0100

    Added logic to copy reward. Added logic to save a new reward.

--- client/src/parent/rewards/RewardsList.tsx
+++ client/src/parent/rewards/RewardsList.tsx
@@ -1,6 +1,5 @@
 import {
   Box,
-  CircularProgress,
   Paper,
   makeStyles,
   createStyles,
@@ -9,12 +8,14 @@ import {
 } from "@material-ui/core";
 import { Fab } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
-import ErrorIcon from "@material-ui/icons/Error";
 import { AddRewardDialog } from "./AddRewardDialog";
 import { useState } from "react";
 import { RewardCard } from "./RewardCard";
 import { useDashboardTitle } from "../../shell/hooks/DashboardTitleHook";
 import { useChildrenDataForUser, useParentRewardData } from "../BackendAccess";
+import { RewardWithGrants } from "../models/RewardWithGrants";
+import produce from "immer";
+import { useParentRewardPost } from "../../api/BackendAccess";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -34,46 +35,47 @@ export function RewardsList() {
   useDashboardTitle("Belohnungspinnwand");
   const classes = useStyles();
   const [showDialog, setShowDialog] = useState(false);
-  const {
-    isLoading: isChildrenLoading,
-    error: childrenError,
-    children,
-  } = useChildrenDataForUser();
-  const { isLoading, error, rewards } = useParentRewardData();
+  const [rewardToEdit, setRewardToEdit] = useState<RewardWithGrants>();
+  const { rewards } = useParentRewardData();
+  const addReward = useParentRewardPost();
+  const { children } = useChildrenDataForUser();
 
   function handleOnCancel() {
-    setShowDialog(false); // TODO js (02.03.2021): Replace dummy implementation with correct cancel logic.
+    setRewardToEdit(undefined);
+    setShowDialog(false);
   }
 
-  function handleOnSave() {
-    setShowDialog(false); // TODO js (02.03.2021): Replace dummy implementation with correct save logic.
+  function handleOnSave(reward: RewardWithGrants) {
+    addReward.mutate({
+      url: "/api/Rewards/",
+      object: reward,
+    });
+    setShowDialog(false);
   }
 
-  function handleRewardChore() {
+  function handleAddReward() {
     setShowDialog(true);
   }
 
-  if (isLoading || isChildrenLoading)
-    return (
-      <Box>
-        <p>Loading...</p>
-        <CircularProgress />
-      </Box>
-    ); // TODO js (11.03.2021): Implement more sophisticated loading screen. Refactor to general loading screen/overlay?
-
-  if (error || childrenError)
-    return (
-      <Box>
-        <ErrorIcon color="secondary" fontSize="large" />
-        <p>{`An error has occurred: ${error}`}</p>
-      </Box>
-    ); // TODO js (11.03.2021): Implement more sophisticated error screen. Refactor to general error screen?
+  function handleCopyReward(reward: RewardWithGrants) {
+    const rewardCopy = produce(reward, (draftReward: RewardWithGrants) => {
+      draftReward.id = 0;
+      draftReward.grants = [];
+    });
+    setRewardToEdit(rewardCopy);
+    setShowDialog(true);
+  }
 
   return (
     <Box className={classes.container} component={Paper}>
       <List>
         {rewards?.map((reward) => (
-          <RewardCard key={reward.id} reward={reward} children={children} />
+          <RewardCard
+            key={reward.id}
+            reward={reward}
+            children={children}
+            onCopyReward={handleCopyReward}
+          />
         ))}
       </List>
       <Fab
@@ -81,12 +83,13 @@ export function RewardsList() {
         size="small"
         color="primary"
         aria-label="add"
-        onClick={handleRewardChore}
+        onClick={handleAddReward}
       >
         <AddIcon />
       </Fab>
       <AddRewardDialog
         open={showDialog}
+        reward={rewardToEdit}
         onCancel={handleOnCancel}
         onSave={handleOnSave}
       />
