0998805ffa1b69c395d03133fd90fa6c4b74bb69
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-30 22:18:36 +0200

    Sort rewards by modification date.

--- client/src/parent/models/RewardWithGrants.ts
+++ client/src/parent/models/RewardWithGrants.ts
@@ -7,3 +7,28 @@ export interface RewardWithGrants {
   value: number;
   grants: Grant[];
 }
+
+function compareGrants(a: Grant[], b: Grant[]): number {
+  if (a.length === 0 || b.length === 0) {
+    return a.length - b.length;
+  }
+
+  const aInMs = Math.max(
+    ...a.map((item) => (item.modified ? new Date(item.modified).getTime() : 0))
+  );
+  const bInMs = Math.max(
+    ...b.map((item) => (item.modified ? new Date(item.modified).getTime() : 0))
+  );
+
+  return aInMs - bInMs;
+}
+
+export function compareChores(
+  a: RewardWithGrants,
+  b: RewardWithGrants
+): number {
+  const grantComparison = compareGrants(a.grants, b.grants);
+  if (grantComparison !== 0) return grantComparison;
+
+  return a.name.localeCompare(b.name);
+}
