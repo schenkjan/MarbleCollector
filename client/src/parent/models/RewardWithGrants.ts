9782a26980f16d4b4cea38f52a70a18cdc657a29
Jan Schenk <Jan.Schenk@swisscom.com>
2021-03-30 23:02:22 +0200

    Refined sorting algorithm for chores and rewards.

--- client/src/parent/models/RewardWithGrants.ts
+++ client/src/parent/models/RewardWithGrants.ts
@@ -1,4 +1,5 @@
 import { Grant } from "./Grant";
+import { isParentActionNeeded } from "./GrantState";
 
 export interface RewardWithGrants {
   id: number;
@@ -8,19 +9,29 @@ export interface RewardWithGrants {
   grants: Grant[];
 }
 
+function maxTime(grants: Grant[]): number {
+  return Math.max(
+    ...grants.map((item) =>
+      item.modified ? new Date(item.modified).getTime() : 0
+    )
+  );
+}
+
 function compareGrants(a: Grant[], b: Grant[]): number {
   if (a.length === 0 || b.length === 0) {
     return a.length - b.length;
   }
 
-  const aInMs = Math.max(
-    ...a.map((item) => (item.modified ? new Date(item.modified).getTime() : 0))
-  );
-  const bInMs = Math.max(
-    ...b.map((item) => (item.modified ? new Date(item.modified).getTime() : 0))
-  );
+  const aActive = a.filter((item) => isParentActionNeeded(item.state));
+  const bActive = b.filter((item) => isParentActionNeeded(item.state));
 
-  return aInMs - bInMs;
+  if (aActive.length !== bActive.length) return aActive.length - bActive.length;
+
+  const activeTimeComparison = maxTime(aActive) - maxTime(bActive);
+
+  if (activeTimeComparison !== 0) return activeTimeComparison;
+
+  return maxTime(a) - maxTime(b);
 }
 
 export function compareChores(
