d7d3f43b714bf0cd396755cb9e14711799f74dd3
David <vid_aesch@hotmail.com>
2021-04-01 11:31:04 +0200

    Added the distinction between development and demo data... created useful (?) demo data and revised the envisioned demo story in the onenote

---
+++ api/MarbleCollectorApi/Data/Init/IDataSeeder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MarbleCollectorApi.Data.Init
+{
+    public interface IDataSeeder<T>
+    {
+        IEnumerable<T> GetDemoData();
+        IEnumerable<T> GetDevelopmentData();
+    }
+}
