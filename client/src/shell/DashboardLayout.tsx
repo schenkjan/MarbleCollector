adffaa17afd5da87dcd52916c99fea8f67a9b597
Jan Schenk <Jan.Schenk@swisscom.com>
2021-04-02 12:41:12 +0200

    Introduced SwipeNavigator to be able to move between pages using swipe gesture.

--- client/src/shell/DashboardLayout.tsx
+++ client/src/shell/DashboardLayout.tsx
@@ -8,6 +8,7 @@ import { ShowSnack } from "./Snackbar";
 import { TitleBar } from "./title/TitleBar";
 import BackdropOverlay from "./BackdropOverlay";
 import { DashboardNotificationHandler } from "./DashboardNotificationHandler";
+import { SwipeNavigator } from "./navigation/SwipeNavigator";
 
 const useStyles = makeStyles({
   boxRoot: {
@@ -31,23 +32,25 @@ export function DashboardLayout(props: Props) {
   const userIsAuthenticated = useRecoilValue(AppState.userIsAuthenticated);
 
   return (
-    <Box
-      className={classes.boxRoot}
-      display="flex"
-      flexDirection="column"
-      justifyContent="space-between"
-    >
-      <DashboardNotificationHandler />
-      <TitleBar
-        avatarAlt={userAvatarInfo.imgAlt}
-        avatarSrc={userAvatarInfo.imgSrc}
-        userIsAuthenticated={userIsAuthenticated}
-        title={dashboardTitle}
-      />
-      <div className={classes.children}>{props.children}</div>
-      <NavigationBar showNavigationBar={userIsAuthenticated} />
-      <ShowSnack />
-      <BackdropOverlay />
-    </Box>
+    <SwipeNavigator>
+      <Box
+        className={classes.boxRoot}
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+      >
+        <DashboardNotificationHandler />
+        <TitleBar
+          avatarAlt={userAvatarInfo.imgAlt}
+          avatarSrc={userAvatarInfo.imgSrc}
+          userIsAuthenticated={userIsAuthenticated}
+          title={dashboardTitle}
+        />
+        <div className={classes.children}>{props.children}</div>
+        <NavigationBar showNavigationBar={userIsAuthenticated} />
+        <ShowSnack />
+        <BackdropOverlay />
+      </Box>
+    </SwipeNavigator>
   );
 }
