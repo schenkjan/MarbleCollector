8b0007aa5b1a6253aba735cf8efdc150a1fa2a8f
Jan Schenk <Jan.Schenk@swisscom.com>
2021-04-02 07:36:23 +0200

    Added dark mode switch and its state persistance.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,22 +2,19 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HomeScreen } from "./home/HomeScreen";
-import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { AuthController } from "./auth/AuthController";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ProtectedRoute } from "./auth/ProtectedRoute";
 import { ProtectedRoutesController } from "./auth/ProtectedRoutesController";
 import { DashboardLayout } from "./shell/DashboardLayout";
 import { HubConnectionHandler } from "./notifications/HubConnectionHandler";
+import { useTheme } from "./shell/hooks/DarkThemeHook";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const theme = createMuiTheme({
-    palette: {
-      type: "light",
-    },
-  });
+  const theme = useTheme();
 
   return (
     <Router>
